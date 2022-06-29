623e36656b1fcf820ced50901b2f38ae245d808d
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-06-29 16:46:08 +0530

    4 commit

--- 2.py
+++ 2.py
@@ -1,8 +1,6 @@
-# new content file 
 AWS_REGION = "us-east-1"
 ENV = "dev"
 PIPELINE_NAME = "dataengg"
-# new content file 
 dataDic = {
     f"invh_sdlf_{ENV}_{PIPELINE_NAME}_curated_db": {
         "LFTagKey": "module",
@@ -13,5 +11,3 @@ dataDic = {
         "LFTagValues": ['curated']
     }
 }
-# new content file 
-# hvjslfgfyrgfvru
