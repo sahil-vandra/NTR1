752c345f7a197c43a16601e41e070fb37c126ceb
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-06-29 16:47:15 +0530

    4 commit

--- 4.py
+++ 4.py
@@ -1,14 +1,11 @@
 import json
-# hvjslfgfyrgfvru
 AWS_REGION = "us-east-1"
 ENV = "dev"
 PIPELINE_NAME = "dataengg"
-# hvjslfgfyrgfvru
 f = open('json_data.json')
 data = json.load(f)
 print(data)
 f.close()
-# hvjslfgfyrgfvru
 for key, value in data.items():
     print(key.format(ENV, PIPELINE_NAME))
     print(
