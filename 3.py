ee6829fb91b2f6109a1ca378a8099cbeba24078f
sahil-vandra <sahilvandra.softvan@gmail.com>
2022-06-29 17:27:38 +0530

    a

---
+++ 3.py
@@ -0,0 +1,15 @@
+import json
+
+AWS_REGION = "us-east-1"
+ENV = "dev"
+PIPELINE_NAME = "dataengg"
+
+f = open('json_data.json')
+data = json.load(f)
+print(data)
+f.close()
+
+for key, value in data.items():
+    print(key.format(ENV, PIPELINE_NAME))
+    print(
+        f"Successfully assign {value['LFTagKey']} Tag Key and {value['LFTagValues']} Tag Value to {key.format(ENV, PIPELINE_NAME)} database")
